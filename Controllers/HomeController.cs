77323a9107cd6fb5c38813ce6ee10804f0e4a34c
Balkarn <46694610+balkarnsingh@users.noreply.github.com>
2019-10-21 21:01:01 -0400

    Controllers scaffolded

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,28 +4,14 @@ using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using ASP_Assignment.Models;
+using NetCoreAssignment.Models;
 
-namespace ASP_Assignment.Controllers
+namespace NetCoreAssignment.Controllers
 {
     public class HomeController : Controller
     {
         public IActionResult Index()
         {
-            return View();
-        }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
