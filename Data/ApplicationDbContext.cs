6003cb8f18b9287a5b2ca2930dfa449350240320
Balkarn <46694610+balkarnsingh@users.noreply.github.com>
2019-10-21 21:04:19 -0400

    dbsets created and added a migration

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Assignment.Models;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
-namespace ASP_Assignment.Data
+namespace NetCoreAssignment.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public virtual DbSet<Movie> Movies { get; set; }
+        public virtual DbSet<Cast> Casts { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- Data/Migrations/00000000000000_CreateIdentitySchema.cs
+++ Data/Migrations/00000000000000_CreateIdentitySchema.cs
@@ -2,7 +2,7 @@
 using Microsoft.EntityFrameworkCore.Metadata;
 using Microsoft.EntityFrameworkCore.Migrations;
 
-namespace ASP_Assignment.Data.Migrations
+namespace NetCoreAssignment.Data.Migrations
 {
     public partial class CreateIdentitySchema : Migration
     {
