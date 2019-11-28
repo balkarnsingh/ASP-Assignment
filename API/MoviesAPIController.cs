f5e1c80f13ef37a977ff615c35be0800de9e770d
Balkarn <balkarnsinghdk@gmail.com>
2019-11-28 13:30:45 +0300

    created movieapi controller

---
+++ API/MoviesAPIController.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_Assignment.Data;
+using Assignment.Models;
+
+namespace ASP_Assignment.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesAPIController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MoviesAPIController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/MoviesAPI
+        [HttpGet]
+        public IEnumerable<Movie> GetMovies()
+        {
+            return _context.Movies;
+        }
+
+        // GET: api/MoviesAPI/5
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetMovie([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(movie);
+        }
+
+        // PUT: api/MoviesAPI/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutMovie([FromRoute] int id, [FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != movie.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/MoviesAPI
+        [HttpPost]
+        public async Task<IActionResult> PostMovie([FromBody] Movie movie)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetMovie", new { id = movie.Id }, movie);
+        }
+
+        // DELETE: api/MoviesAPI/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie([FromRoute] int id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return Ok(movie);
+        }
+
+        private bool MovieExists(int id)
+        {
+            return _context.Movies.Any(e => e.Id == id);
+        }
+    }
+}
