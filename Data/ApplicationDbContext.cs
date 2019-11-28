6a04e5021a11c476ed301494e3de791e7a75e828
Balkarn <balkarnsinghdk@gmail.com>
2019-11-28 11:38:19 +0300

    created applicationuser and applicationrole

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -2,12 +2,13 @@
 using System.Collections.Generic;
 using System.Text;
 using Assignment.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace ASP_Assignment.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, string>
     {
         public virtual DbSet<Movie> Movies { get; set; }
         public virtual DbSet<Cast> Casts { get; set; }
@@ -16,5 +17,17 @@ namespace ASP_Assignment.Data
             : base(options)
         {
         }
+    }
+
+    public class ApplicationUser: IdentityUser<string>
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+
+    public class ApplicationRole: IdentityRole<string>
+    {
+        public string RoleDescription { get; set; }
     }
 }
